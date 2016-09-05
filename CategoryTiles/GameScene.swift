c939054c4c8b55ca5194f226d966d4fc0153f8e7
Michael Sylva <michaelsylva@michaels-mini.example.com>
2016-09-04 23:23:59 -0400

    Initial Commit

---
+++ CategoryTiles/GameScene.swift
@@ -0,0 +1,67 @@
+//
+//  GameScene.swift
+//  CategoryTiles
+//
+//  Created by Michael Sylva on 9/3/16.
+//  Copyright (c) 2016 Michael Sylva. All rights reserved.
+//
+
+import SpriteKit
+import CoreMotion
+
+var tiles = [Tile]()
+var motionManager = CMMotionManager()
+var destX:CGFloat = 0.0
+
+class Tile{
+    var sprite : SKSpriteNode
+    
+    init(sprite: SKSpriteNode){
+        self.sprite = sprite
+    }
+}
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        
+        self.size = view.bounds.size //set the size to the view size
+        
+        let t = createTile("x")
+        
+        let t2 = createTile("x")
+        t2.sprite.position.x+=50
+        
+        let t3 = createTile("x")
+        t3.sprite.position.x-=50
+        t3.sprite.position.y+=20
+        
+        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
+        self.physicsBody?.restitution=0.25 //adds slight bounciness
+        
+        motionManager.startAccelerometerUpdates()
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+      
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+        if let accData = motionManager.accelerometerData{
+            physicsWorld.gravity = CGVector(dx: accData.acceleration.x * 2, dy: accData.acceleration.y * 2)
+        }
+    }
+    
+    func createTile(letter : String) -> Tile{
+        let sprite = SKSpriteNode(imageNamed: "tile.png")
+        sprite.position = CGPointMake(frame.size.width/2,frame.size.height/2)
+        sprite.physicsBody = SKPhysicsBody(rectangleOfSize: sprite.size)
+        sprite.physicsBody?.dynamic = true
+        self.addChild(sprite)
+        
+        let t = Tile(sprite:sprite)
+        tiles.append(t)
+        return t
+    }
+}
---
+++ CategoryTiles/Tile.swift
@@ -0,0 +1,9 @@
+//
+//  Tile.swift
+//  CategoryTiles
+//
+//  Created by Michael Sylva on 9/4/16.
+//  Copyright © 2016 Michael Sylva. All rights reserved.
+//
+
+import Foundation

ad11b61aa3b42ce9a5505845ac950b94dfd2f03e
Michael Sylva <michaelsylva@michaels-mini.example.com>
2016-09-07 10:00:48 -0400

    added second subcategory on menu, (just to make sure both work)

--- MenuScene.swift
+++ MenuScene.swift
@@ -13,16 +13,28 @@ class MenuScene: SKScene {
     override func didMoveToView(view: SKView) {
         backgroundColor = UIColor.whiteColor()
         self.size = view.bounds.size
-        let start = SKLabelNode(text: "Sports / Hockey");
-        start.fontColor = UIColor.blackColor()
-        start.position = CGPointMake(view.bounds.width/2, view.bounds.height/2)
-        start.userData = [
+        let hockey = SKLabelNode(text: "Sports / Hockey");
+        hockey.fontColor = UIColor.blackColor()
+        hockey.position = CGPointMake(view.bounds.width/2, view.bounds.height/2)
+        hockey.userData = [
             "category" : "Sports",
             "subcategory" : "Hockey",
             "words" : "stick,puck,goalie,penalty",
             "destination" : "gameboard"
         ]
-        self.addChild(start)
+        
+        let basketball = SKLabelNode(text: "Sports / Basketball");
+        basketball.fontColor = UIColor.blackColor()
+        basketball.position = CGPointMake(view.bounds.width/2, view.bounds.height/2-100)
+        basketball.userData = [
+            "category" : "Sports",
+            "subcategory" : "Basketball",
+            "words" : "rim,guard,zone,rebound",
+            "destination" : "gameboard"
+        ]
+        
+        self.addChild(hockey)
+        self.addChild(basketball)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
